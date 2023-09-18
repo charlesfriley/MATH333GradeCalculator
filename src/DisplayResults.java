4502406282e9d8b6994dcbcf2e63f99b202ef801
Charles Friley <charlie21504@gmail.com>
2023-09-18 01:12:00 -0600

    created a test panel to test display

--- src/DisplayResults.java
+++ src/DisplayResults.java
@@ -1,10 +1,67 @@
-public class DisplayResults {
+import javax.swing.*;
+
+public class DisplayResults extends JPanel{
     
     /* instance variables */
+    String[] columnNames;
+    Object[][] data;
+    JTable table;
+    JScrollPane scrollPane;
 
     /* constructer */
-    public DisplayResults()
+    public DisplayResults() 
     {
+        columnNames = new String[] {"Final Grade", //TODO put teh jtable main display in another file.
+                                   "Quiz Weight",
+                                   "Test Weight",
+                                   "Final Weight",
+                                   "Resulting Grade"};
+
+        data = new Object[][] { //TODO change to use the calculator class
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        {"1Test", "two", (Integer) 3, (Integer) 4, "1Grade"},
+        {"2Test", "three", (Integer)5, (Integer) 6, "2Grade"},
+        {"3Test", "four", (Integer)7, (Integer) 8, "3Grade"},
+        };
+
+        table = new JTable(data, columnNames);
+
+
+        scrollPane = new JScrollPane(table);
+
+        this.add(scrollPane);
 
     }
 
