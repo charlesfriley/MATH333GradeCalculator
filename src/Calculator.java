267345e0d62a28d916252446b536e32cbf13f829
Charles <134988367+charlesfriley@users.noreply.github.com>
2023-09-18 12:21:46 -0600

    Update comments on Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,12 @@ public class Calculator {
  * TODO
  * must calculate the stuff
  * must return an Object[][]
+ * This class should take in the user quiz grade and user test grade, then calculate all the
+ * best grades for if the final grade is 5%, 10%, 15%... 95%, 100%. the object[][] would look something like:
+ *  {"Final Grade", "Quiz Weight", "Test Weight", "Final Weight", "Resulting Grade"},
+ *  {"Final Grade2", "Quiz Weight2", "Test Weight2", "Final Weight2", "Resulting Grade2"},
+ *  ...
+ * 
  * 
  */
 
