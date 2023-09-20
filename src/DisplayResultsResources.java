75f6691c5292118315eef3994e4ed661cc8287ad
Charles Friley <charlie21504@gmail.com>
2023-09-20 13:27:14 -0600

    update comments

--- src/DisplayResultsResources.java
+++ src/DisplayResultsResources.java
@@ -3,5 +3,10 @@ public class DisplayResultsResources {
      * This file should orginize results 
      * found from calculator class and send them over to teh DisplayResults file.
      * 
+     * 
+     * 
+     * // basicly this file needs to request a calculation from calculator.java at a certain final, quiz, and test grade. 
+     * // then it will request another, and another, and another
+     * // it will orginize all this code into a Object[][] that the table in DisplayResults.java can use.
      */
 }
