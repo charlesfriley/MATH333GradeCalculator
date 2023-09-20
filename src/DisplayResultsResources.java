13c5b596d24b64b28cb0554a03fef9b8f011aec9
Charles Friley <charlie21504@gmail.com>
2023-09-20 13:24:27 -0600

    Created file to orginize use of Calculator.java

---
+++ src/DisplayResultsResources.java
@@ -0,0 +1,7 @@
+public class DisplayResultsResources {
+    /*
+     * This file should orginize results 
+     * found from calculator class and send them over to teh DisplayResults file.
+     * 
+     */
+}
