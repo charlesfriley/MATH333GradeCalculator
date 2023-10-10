ee2569d7a0608e74f93e1a6bb933238f1b8dfc8a
Charles Friley <charlie21504@gmail.com>
2023-10-09 23:55:22 -0600

    added all buttons and combo boxes, unusable

--- src/DisplayResults.java
+++ src/DisplayResults.java
@@ -1,28 +1,39 @@
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.Cursor;
 
 
 public class DisplayResults extends JPanel{
     
-    /* instance variables */
+    /* instance variables */ //TODO make this public or private respectuvly
 
-    String[] columnNames;
-    Object[][] data;
-    JTable table;
+    private String[] columnNames;
+    private Object[][] data;
+
+    // center panel
+    private JTable table;
     /**
      * scrollPane contanes the table and buttons
      */
-    JScrollPane scrollPane;
-    JPanel buttonsPanel, dataAndButtonPanel;
+    private JScrollPane scrollPane;
 
-    JButton testButton;
+
+
+    // south panel
+    private JPanel southPanel, comboBoxPanel, customValuesPanel;
+    private String[] comboArray = {"-", "0%-5%", "5%-10%", "10%-15%", "15%-20%", "20%-25%", 
+    "25%-30%", "30%-35%", "35%-40%", "40%-45%", "45%-50%", "50%-55%", "55%,60%","60%-65%", 
+    "65%-70%", "70%-75%", "75%-80%", "80%-85%", "85%-90%", "90%-95%", "95%-100%",};
+    private JComboBox selectionBox;
+    private JButton newTableButton, customButton;
+    private JLabel comboLabel, customValuesLabel;
+    private JTextField customTextField;
+
 
     /* constructer */
     public DisplayResults() 
     {
         this.setLayout(new BorderLayout());
-        testButton = new JButton("Test Button");
-
 
         /* format and create table */
         columnNames = new String[] {"Final Grade", //TODO put teh jtable main display in another file.
@@ -108,16 +119,50 @@ public class DisplayResults extends JPanel{
 
         table = new JTable(data, columnNames);
 
-        /* create buttons panel */
-        buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
-        buttonsPanel.add(testButton);
+        /* create combo box panel */
+        comboBoxPanel = new JPanel();
+        comboBoxPanel.setLayout(new BoxLayout(comboBoxPanel, BoxLayout.X_AXIS));
+        
+        selectionBox = new JComboBox(comboArray);
+        selectionBox.setToolTipText("Select a range of potential final grade values and press the button to the right");
+        selectionBox.setMaximumSize(selectionBox.getPreferredSize());
+        selectionBox.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        comboLabel = new JLabel("Display table in range of:  ");
 
-        /* create buttons + data tabel panel */
-        dataAndButtonPanel = new JPanel();
-        dataAndButtonPanel.setLayout(new BoxLayout(dataAndButtonPanel, BoxLayout.X_AXIS));
-        dataAndButtonPanel.add(buttonsPanel);
-        dataAndButtonPanel.add(table);
+        newTableButton = new JButton("Display Table"); //TODO add listener, this button should open a new window with a table of values between specified amount in combo cox
+        newTableButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+
+        comboBoxPanel.add(Box.createHorizontalStrut(15));
+        comboBoxPanel.add(comboLabel);
+        comboBoxPanel.add(selectionBox);
+        comboBoxPanel.add(newTableButton);
+
+        /* create custom value panel */
+        customValuesPanel = new JPanel();
+        customValuesPanel.setLayout(new BoxLayout(customValuesPanel, BoxLayout.X_AXIS));
+
+        customValuesLabel = new JLabel("Enter a specific final grade (##.##)   ");
+
+        customTextField = new JTextField(5); //TODO add listener for if the user presses enter AND add a check if the user entered good values similar to what was done in teh main menu.
+        customTextField.setMaximumSize(customTextField.getPreferredSize()); 
+        customTextField.setMinimumSize(customTextField.getPreferredSize());
+
+        customButton = new JButton("Calculate"); //TODO add listener and this button should calculate a certain value and either open it in a new panel OR add it to the south panel and repaint the panel. I like the second idea better. If i do that then the pannel should dissapear if the user types in nothing at all and presses enter.
+        customButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        customValuesPanel.add(Box.createHorizontalStrut(15));
+        customValuesPanel.add(customValuesLabel);
+        customValuesPanel.add(customTextField);
+        customValuesPanel.add(customButton);
+        customValuesPanel.add(Box.createHorizontalStrut(15));
+
+
+        /* create South panel */
+        southPanel = new JPanel();
+        southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.Y_AXIS));
+        southPanel.add(comboBoxPanel);
+        southPanel.add(customValuesPanel);
 
 
 
@@ -130,10 +175,11 @@ public class DisplayResults extends JPanel{
         // manual adding will be done on the bottom layout
 
         // __________________________
-        scrollPane = new JScrollPane(dataAndButtonPanel);
+        scrollPane = new JScrollPane(table);
 
         this.add(scrollPane, BorderLayout.CENTER);
-
+        this.add(southPanel, BorderLayout.SOUTH);
+        
         // this.add(dataAndButtonPanel);
 
 
