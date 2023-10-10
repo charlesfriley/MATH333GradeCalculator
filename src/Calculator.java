ff335f479eaa8a0a5ca3687f0be4c1987e2b1d07
Charles Friley <charlie21504@gmail.com>
2023-10-09 23:52:59 -0600

    updated comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,16 +7,10 @@ public class Calculator {
 /*
  * TODO
  * must calculate the stuff
- * must return an Object[][]
- * This class should take in the user quiz grade and user test grade, then calculate all the
- * best grades for if the final grade is 5%, 10%, 15%... 95%, 100%. the object[][] would look something like:
- *  {"Final Grade", "Quiz Weight", "Test Weight", "Final Weight", "Resulting Grade"},
- *  {"Final Grade2", "Quiz Weight2", "Test Weight2", "Final Weight2", "Resulting Grade2"},
- *  ...
  * 
  * Here is how i think this file will work: You will request a single line of information for the chart 
  *  so this file needs 3 inputs, your final grade, quiz grade, and test grade
- *  it will output an array of objects to be put into the final object[][] that the data table uses
+ *  it will return the best grade possible
  * 
  */
 
