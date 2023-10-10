afb4c3b04deec896fd7eeb084c26e5e1f8a7871d
Charles Friley <charlie21504@gmail.com>
2023-10-09 23:53:29 -0600

    updated comments

--- src/DisplayResultsResources.java
+++ src/DisplayResultsResources.java
@@ -3,7 +3,11 @@ public class DisplayResultsResources {
      * This file should orginize results 
      * found from calculator class and send them over to teh DisplayResults file.
      * 
-     * 
+     * This class should use the calculator class to get a single line of info, then put all the info tegether. 
+     * the object[][] would look something like:
+     *  {"Final Grade", "Quiz Weight", "Test Weight", "Final Weight", "Resulting Grade"},
+     *  {"Final Grade2", "Quiz Weight2", "Test Weight2", "Final Weight2", "Resulting Grade2"},
+     *  ...
      * 
      * // basicly this file needs to request a calculation from calculator.java at a certain final, quiz, and test grade. 
      * // then it will request another, and another, and another
